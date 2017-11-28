71c6842d157cfbc55dbaebad17fd39c6ac2055af
connireimann <33958906+connireimann@users.noreply.github.com>
2017-11-28 15:07:17 +0100

    Ad

---
+++ settings/firebaseInfo.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+ 
+    var config = {
+
+      apiKey: "AIzaSyAfaP8iyYQEv1k4vU3lBo_K2FaNjN6S_5E",
+      authDomain: "agrarpolitik2020.firebaseapp.com",
+      databaseURL: "https://agrarpolitik2020.firebaseio.com",
+      projectId: "agrarpolitik2020",
+      storageBucket: "agrarpolitik2020.appspot.com",
+      messagingSenderId: "514903138744"
+
+    };
+    
+firebase.initializeApp(config);
+    
+var rootRef = firebase.database().ref();
